74db4f9a835287a0befbc9494225aab3db326cd7
Julian <jdvs_g12@hotmail.com>
2022-05-24 23:40:19 -0500

    fetch-movies-searching

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-let arrMovies = "";
-
-
 async function dataOfMovies(movie) {
+  const url = `http://www.omdbapi.com/?apikey=6e25b9e4&t=${movie}`;
 
-    const url = `http://www.omdbapi.com/?apikey=6e25b9e4&t=${movie}`;
+  const respons = await fetch(`${url}`);
+  const data = await respons.json();
 
-    const respons = await fetch(`${url}`);
-    const data = await respons.json();
-    
-    
-    arrMovies = document.getElementById('cardsContainer')
-    cardsContainer.innerHTML = `<img src=${data.Poster} height='300px'>
+  arrMovies = document.getElementById("cardsContainer");
+  cardsContainer.innerHTML = `<img src=${data.Poster} height='300px'>
                                 <h1>${data.Title}</h1>
                                 <p>${data.Year}</p>
-                                <p>${data.Genre}</p>`
-                                console.log(data);
+                                <p>${data.Genre}</p>`;
+  console.log(data);
+}
+
+function addMovies(){
+  const inputSearch = document.getElementById("search");
+  
+  const movie = {
+    search: inputSearch.value,
+  }
+  dataOfMovies(movie.search);
 
 }
 
-dataOfMovies('arena');
+function searchData() {
+
+  const buttonSerach = document.getElementById("add");
+
+  buttonSerach.addEventListener("click", addMovies);
+}
+searchData();
+
 
 function userNameData() {
   (async () => {
